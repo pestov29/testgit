e9c9d7641a3de90391cc6c498331c032e9f11d60
pestov <ipestov2001@gmail.com>
2022-03-22 22:47:45 +0200

    first commit

---
+++ testGit/testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Ivan Pestov on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // mm 
+    
+}
+
